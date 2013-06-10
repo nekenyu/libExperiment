9b21a0c344ae219303ff880dcb71e4a25c73cae6
Jonathan Seng <jonathan.seng@komododoes.com>
2013-06-10 15:45:15 -0700

    Update doc from master

--- doc/examples.js
+++ doc/examples.js
@@ -1,4 +1,5 @@
 var examples =
 [
-    [ "DoubleLinkedListExample.cpp", "_double_linked_list_example_8cpp-example.html", null ]
+    [ "DoubleLinkedListExample.cpp", "_double_linked_list_example_8cpp-example.html", null ],
+    [ "ListMergeSortExample.cpp", "_list_merge_sort_example_8cpp-example.html", null ]
 ];
